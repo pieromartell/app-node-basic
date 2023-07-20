1cea74d3408741d9e9272e9ed033350a21acb591
Piero Martell Falcon <105953521+Pierom689@users.noreply.github.com>
2023-07-19 23:30:40 -0500

    Segundo Cambio 1.0.2

--- src/controllers/keyboard.controller.ts
+++ src/controllers/keyboard.controller.ts
@@ -1,11 +1,44 @@
-const getKeyBoard = () =>{
+import { Request, Response } from "express"
+import handLeHTTP from "../utils/error.handle";
 
+export const getKeyBoard = (req: Request, res:Response) =>{
+    try {
+        
+    } catch (error) {
+        handLeHTTP(res, 'Error_GET_ITEM');
+    }
+}
+export const getKeyBoards = (req: Request, res:Response) =>{
+try {
+        
+    } catch (error) {
+        handLeHTTP(res, 'Error_GET_ITEMS');
+        
+    }
+}
+export const postKeyBoard = (req: Request, res:Response) =>{
+    try {
+        
+    } catch (error) {
+        handLeHTTP(res, 'Error_POST_ITEMS');
+        
+    }
 }
 
-const getKeyBoards = () =>{
-
+export const updateKeyBoard = (req: Request, res:Response)=>{
+try {
+        
+    } catch (error) {
+        handLeHTTP(res, 'Error_PUT_ITEMS');
+        
+    }
+}
+export const deleteKeyBoard = (req: Request, res:Response)=>{try {
+        
+    } catch (error) {
+        handLeHTTP(res, 'Error_DELETE_ITEMS');
+        
+    }
 }
 
-const updateKeyBoard = ()=>{
-    
-}
+
